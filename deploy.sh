8e1f010876595f55a954adcc89b6063d02b8bea0
Andy Boutte <andyboutte@camerapi.(none)>
2013-07-24 03:47:03 +0000

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#list of files that need to be copied to and from the repository
+
+/var/www/*			#website files
+/usr/lib/cgi-bin/*		#script for executing gphoto from web interface
+/etc/sudoers
+/usr/local/bin/*
